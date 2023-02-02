c9ce351b9a15892495ed25873a6c9bfc087883f2
Diana Samusenka <dianas190999@gmail.com>
2023-02-02 18:44:54 +0300

    Initial Commit

---
+++ RSS_reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSS_reader
+//
+//  Created by Diana Samusenka on 2.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
